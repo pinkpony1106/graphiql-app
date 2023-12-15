1beaea126d1efcd96e1860c8dd90db3e8e6c2b7d
pinkpony1106 <juliaegorova221098@gmail.com>
2023-12-15 14:11:13 +0300

    feat: add animated sticky header

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   verbose: true,
+  testMatch: ['<rootDir>/src/**/*.test.{js,jsx,ts,tsx}'],
   extensionsToTreatAsEsm: ['.jsx, .tsx, .ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   collectCoverage: true,
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,65 +1,85 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './header.module.css';
+import classNames from 'classnames';
 import { useNavigate } from 'react-router-dom';
 
 function Header() {
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
+  const [animatedHeader, setAnimatedHeader] = useState(false);
 
   const handleOpen = () => {
     setOpen(!open);
   };
 
-  return (
-    <div className={styles.container}>
-      <div
-        className={styles.logo}
-        onClick={() => {
-          navigate(`/graph-ql`);
-        }}
-      >
-        <img className={styles.logoImg} src="src/assets/graphql-logo.svg" />
-        <div className={styles.title}>GraphQL</div>
-      </div>
-      <div className={styles.nav}>
-        <div
-          className={styles.home}
-          onClick={() => {
-            navigate(`/`);
-          }}
-        >
-          Home
-        </div>
-        <button
-          className="pinkButton"
-          onClick={() => {
-            navigate(`/signIn`);
-          }}
-        >
-          Sign In
-        </button>
-        <button
-          className="pinkButton"
-          onClick={() => {
-            navigate(`/signUp`);
-          }}
-        >
-          Sign Up
-        </button>
+  useEffect(() => {
+    const headerListener = () => {
+      if (window.scrollY > 0) {
+        setAnimatedHeader(true);
+      } else setAnimatedHeader(false);
+    };
+    window.addEventListener('scroll', headerListener);
+    return () => {
+      window.removeEventListener('scroll', headerListener);
+    };
+  }, []);
 
-        <button className="blackButtonLang" onClick={handleOpen}>
-          {open ? (
-            <>
-              <div>English</div>
-              <img src="src/assets/lang-arrow.svg" />
-            </>
-          ) : (
-            <>
-              <div>Russian</div>
-              <img src="src/assets/lang-arrow.svg" />
-            </>
-          )}
-        </button>
+  return (
+    <div
+      className={classNames(styles.wrapper, {
+        [styles.scroll]: animatedHeader,
+      })}
+    >
+      <div className={styles.container}>
+        <div
+          className={styles.logo}
+          onClick={() => {
+            navigate(`/graph-ql`);
+          }}
+        >
+          <img className={styles.logoImg} src="src/assets/graphql-logo.svg" />
+          <div className={styles.title}>GraphQL</div>
+        </div>
+        <div className={styles.nav}>
+          <div
+            className={styles.home}
+            onClick={() => {
+              navigate(`/`);
+            }}
+          >
+            Home
+          </div>
+          <button
+            className="pinkButton"
+            onClick={() => {
+              navigate(`/signIn`);
+            }}
+          >
+            Sign In
+          </button>
+          <button
+            className="pinkButton"
+            onClick={() => {
+              navigate(`/signUp`);
+            }}
+          >
+            Sign Up
+          </button>
+
+          <button className="blackButtonLang" onClick={handleOpen}>
+            {open ? (
+              <>
+                <div>English</div>
+                <img src="src/assets/lang-arrow.svg" />
+              </>
+            ) : (
+              <>
+                <div>Russian</div>
+                <img src="src/assets/lang-arrow.svg" />
+              </>
+            )}
+          </button>
+        </div>
       </div>
     </div>
   );

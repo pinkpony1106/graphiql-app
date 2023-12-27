a32464d08555e032eab3f4f708a45f99fcfc1478
pinkpony1106 <juliaegorova221098@gmail.com>
2023-12-27 16:18:33 +0300

    feat: add localization to not found page

--- src/Components/404/InvalidRoute.tsx
+++ src/Components/404/InvalidRoute.tsx
@@ -1,16 +1,19 @@
 import { Link } from 'react-router-dom';
 import styles from './InvalidRoute.module.css';
+import { TranslateContext, tKeys } from '../../Context/Context';
+import { useContext } from 'react';
 
 function InvalidRoute() {
+  const { t } = useContext(TranslateContext);
   return (
     <>
       <div className={styles.error}>
         <span className={styles.notfound}>404 </span>
-        <span>Oops!</span>
-        <span>The page you are looking for could not be found! </span>
-        <span>Please, go back to previous page.</span>
+        <span>{t(tKeys.oops)}</span>
+        <span>{t(tKeys.notFound)}</span>
+        <span>{t(tKeys.goBackPrevious)}</span>
         <Link to={'/'}>
-          <button className={styles.goBack}>Go Back</button>
+          <button className={styles.goBack}>{t(tKeys.goBack)}</button>
         </Link>
       </div>
     </>

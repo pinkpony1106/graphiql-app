3abf0242ffb98a8e9de2495449b25a7fb163a83c
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2024-01-07 21:59:04 +0100

    feat: create test for query text editor & check lint

--- src/Components/RequestEditor/_tests_/HeadersEditor.test.tsx
+++ src/Components/RequestEditor/_tests_/HeadersEditor.test.tsx
@@ -1,25 +1,27 @@
-import { fireEvent, render } from "@testing-library/react";
-import { useDispatch } from "react-redux";
-import HeadersEditor from "../HeadersEditor/HeadersEditor";
+import { fireEvent, render } from '@testing-library/react';
+import { useDispatch } from 'react-redux';
+import HeadersEditor from '../HeadersEditor/HeadersEditor';
 
 jest.mock('react-redux', () => ({
-    ...jest.requireActual('react-redux'),
-    useDispatch: jest.fn(),
+  ...jest.requireActual('react-redux'),
+  useDispatch: jest.fn(),
 }));
 
 describe('Headers Editor Component', () => {
-    it('Updates the value of headers when editing content', () => {
-        const mockDispatch = jest.fn();
-        (useDispatch as jest.Mock).mockReturnValue(mockDispatch);
+  it('Updates the value of headers when editing content', () => {
+    const mockDispatch = jest.fn();
+    (useDispatch as jest.Mock).mockReturnValue(mockDispatch);
 
-        const { getByPlaceholderText } = render(<HeadersEditor />);
-        const headersTextArea = getByPlaceholderText('{ your headers }');
+    const { getByPlaceholderText } = render(<HeadersEditor />);
+    const headersTextArea = getByPlaceholderText('{ your headers }');
 
-        fireEvent.input(headersTextArea, { target: { innerText: 'Content-Type: application/json' } });
-
-        expect(mockDispatch).toHaveBeenCalledWith({
-            type: 'headersValue/updateHeadersValue',
-            payload: 'Content-Type: application/json',
-        });
+    fireEvent.input(headersTextArea, {
+      target: { innerText: 'Content-Type: application/json' },
     });
+
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: 'headersValue/updateHeadersValue',
+      payload: 'Content-Type: application/json',
+    });
+  });
 });
